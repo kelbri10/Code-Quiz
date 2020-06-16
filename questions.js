852dad669c10378da3497664ff0ebea55cfaa50c
kelbri10 <kelbri10@gmail.com>
2020-06-16 16:04:41 -0400

    added style.css and buildQuiz() function to questions.js

---
+++ questions.js
@@ -0,0 +1,54 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    }
+  ];
+
+function buildQuiz(){ 
+    var quizDisplayId = document.getElementById('quizDisplay'); 
+
+    for(var i = 0; i < questions.length; i++){ 
+        var quizTitle = document.getElementById('title'); 
+        quizTitle.textContent = questions.title; 
+        
+        for(var i = 0; i < choices.length; i++){ 
+            var quizChoice = document.createElement('button'); 
+            var choiceId = document.getElementById('choices'); 
+
+            quizChoice.textContent = choices[i]; 
+            choiceId.append(quizChoice); 
+        }
+    }
+
+
+}
